9466da2586b28eca11e4224e1b6cf8425a87830d
briceteddy <brice.simo.its@gmail.com>
2024-03-19 12:57:06 +0100

    update 01basicreact and 01basicvite

---
+++ 01basicreact/src/Test.js
@@ -0,0 +1,7 @@
+function Test(){
+    return(
+        <h1>How are you</h1>
+    )
+}
+
+export default Test;
---
+++ 01basicvite/src/Test.jsx
@@ -0,0 +1,8 @@
+function Test(){
+
+    return (
+        <h1>How are you</h1>
+    )
+}
+
+export default Test;
